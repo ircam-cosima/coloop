b643b70cc707dfe6acefe4be51e71234fd7cf5e9
Uros Petrevski <uros@nodesign.net>
2017-05-19 10:39:12 +0200

    Added buttons

--- src/client/player/PlayerRenderer.js
+++ src/client/player/PlayerRenderer.js
@@ -38,14 +38,22 @@ export default class PlayerRenderer extends Canvas2dRenderer {
   render(ctx) {
     ctx.save();
 
-    const canvasMax = Math.max(this.canvasWidth, this.canvasHeight);
-    const r = canvasMax / 5;
-    const stepRadius = r / 6;
+//    const canvasMax = Math.max(this.canvasWidth, this.canvasHeight);
+    
+    const width = this.canvasWidth;
+    const height = this.canvasHeight;
+
+    const buttonW = width/14.0;
+    const buttonH = height/2.84;
+    const segmentW = width/24.0;
+    const segmentH = height/24.0;
+
+//    const r = canvasMax / 5;
+//    const stepRadius = r / 6;
     const states = this.states;
     const numStates = states.length;
-    const yMargin = this.canvasHeight / 2;
-    const xMargin = this.canvasWidth / 2;
-
+//    const yMargin = this.canvasHeight / 2;
+//    const xMargin = this.canvasWidth / 2;
 
     for (let i = 0; i < numStates; i++) {
       let state = states[i];
@@ -63,7 +71,7 @@ export default class PlayerRenderer extends Canvas2dRenderer {
       }
 
 
-      ctx.beginPath();
+//      ctx.beginPath();
       ctx.globalAlpha = 1;
 
       switch (state) {
@@ -94,14 +102,34 @@ export default class PlayerRenderer extends Canvas2dRenderer {
 
       }
 
-      ctx.ellipse(xMargin + this.positionXArr[i], yMargin - this.positionYArr[i], stepRadius, stepRadius, 0, 0, 2 * Math.PI);
+      var x = (i<8) ? segmentW+1.26*buttonW*i : segmentW+1.26*buttonW*(i-8);
+      var y = (i<8) ? (segmentH*3) : (segmentH*3)+1.1*buttonH;
+
+      this.roundRect(x,y, buttonW, buttonH, 7, ctx);
+      //ctx.ellipse(xMargin + this.positionXArr[i], yMargin - this.positionYArr[i], stepRadius, stepRadius, 0, 0, 2 * Math.PI);
       ctx.fill();
       ctx.stroke();
-      ctx.closePath();
+     // ctx.closePath();
     }
 
     ctx.restore();
 
+  }
+  
+  roundRect(x, y, w, h, radius, context){ // radius is corner radius
+    var r = x + w;
+    var b = y + h;
+    context.beginPath();
+    context.moveTo(x+radius, y);
+    context.lineTo(r-radius, y);
+    context.quadraticCurveTo(r, y, r, y+radius);
+    context.lineTo(r, y+h-radius);
+    context.quadraticCurveTo(r, b, r-radius, b);
+    context.lineTo(x+radius, b);
+    context.quadraticCurveTo(x, b, x, b-radius);
+    context.lineTo(x, y+radius);
+    context.quadraticCurveTo(x, y, x+radius, y);
+    context.stroke();
   }
 
   setHighlight(index) {
