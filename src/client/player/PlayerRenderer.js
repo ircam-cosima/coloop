663f967b481a533c4d05e70714de474904499b10
Uros Petrevski <uros@nodesign.net>
2017-05-19 11:55:16 +0200

    added vizualisation

--- src/client/player/PlayerRenderer.js
+++ src/client/player/PlayerRenderer.js
@@ -29,6 +29,34 @@ export default class PlayerRenderer extends Canvas2dRenderer {
       this.positionXArr.push(initPositionX(i));
       this.positionYArr.push(initPositionY(i));
     }
+    
+    this.buttonW = this.canvasWidth/14.0;
+    this.buttonH = this.canvasHeight/2.84;
+    this.segmentW = this.canvasWidth/24.0;
+    this.segmentH = this.canvasHeight/24.0;
+
+    /// INIT POINTS FOR DRAWING CIRCLE
+    this.circlePointList = new Array();
+
+    const tSteps = (Math.PI*2.0)/24.0;
+    var radius = (this.segmentW*4)/2.0;
+
+    var ln = 5;
+
+    for (var i=0; i<4; i++) {
+     var cnt = 0;
+     var circles = new Array();
+     for (var t = 0; t<Math.PI*2.0; t+=tSteps) {
+       var x = this.segmentW*20.9 + radius*Math.cos(t);
+       var y = this.segmentH*8 + radius*Math.sin(t);
+       circles.push({"x":x, "y":y});
+       cnt++;
+     }
+     this.circlePointList.push(circles);
+     radius-=this.segmentH*0.5;
+    }
+    
+    
   }
 
   update(dt) {
@@ -39,42 +67,36 @@ export default class PlayerRenderer extends Canvas2dRenderer {
     ctx.save();
 
 //    const canvasMax = Math.max(this.canvasWidth, this.canvasHeight);
-    
-    const width = this.canvasWidth;
-    const height = this.canvasHeight;
-
-    const buttonW = width/14.0;
-    const buttonH = height/2.84;
-    const segmentW = width/24.0;
-    const segmentH = height/24.0;
 
 //    const r = canvasMax / 5;
 //    const stepRadius = r / 6;
     const states = this.states;
     const numStates = states.length;
+
+    var selector = -1;
 //    const yMargin = this.canvasHeight / 2;
 //    const xMargin = this.canvasWidth / 2;
-
     for (let i = 0; i < numStates; i++) {
-      let state = states[i];
+        let state = states[i];
 
-      if (i === this.highlight && state == 0) {
-       state = 3;
-      }
+        if (i === this.highlight && state == 0) {
+            state = 3;
+        }
 
-      if (i === this.highlight && state == 1) {
-       state = 4;
-      }
+        if (i === this.highlight && state == 1) {
+            state = 4;
+        }
 
-      if (i === this.highlight && state == 2) {
-       state = 4;
-      }
+        if (i === this.highlight && state == 2) {
+            state = 4;
+            
+        }
 
 
-//      ctx.beginPath();
-      ctx.globalAlpha = 1;
+        //      ctx.beginPath();
+        ctx.globalAlpha = 1;
 
-      switch (state) {
+        switch (state) {
         case 0:
           ctx.fillStyle = '#000000';
           ctx.strokeStyle = "#ffffff";
@@ -93,27 +115,49 @@ export default class PlayerRenderer extends Canvas2dRenderer {
         case 3:
           ctx.fillStyle = '#606060';
           ctx.strokeStyle = "#ffffff";
+          selector = i;
           break;
 
         case 4:
           ctx.fillStyle = '#FFFFFF';
           ctx.strokeStyle = "#ffffff";
+          
           break;
 
-      }
+        }
 
-      var x = (i<8) ? segmentW+1.26*buttonW*i : segmentW+1.26*buttonW*(i-8);
-      var y = (i<8) ? (segmentH*3) : (segmentH*3)+1.1*buttonH;
+        var x = (i<8) ? this.segmentW+1.26*this.buttonW*i : this.segmentW+1.26*this.buttonW*(i-8);
+        var y = (i<8) ? (this.segmentH*3) : (this.segmentH*3)+1.1*this.buttonH;
 
-      this.roundRect(x,y, buttonW, buttonH, 7, ctx);
-      //ctx.ellipse(xMargin + this.positionXArr[i], yMargin - this.positionYArr[i], stepRadius, stepRadius, 0, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.stroke();
-     // ctx.closePath();
+        this.roundRect(x,y, this.buttonW, this.buttonH, 7, ctx);
+        //ctx.ellipse(xMargin + this.positionXArr[i], yMargin - this.positionYArr[i], stepRadius, stepRadius, 0, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
     }
 
+    //console.log(selector);
+    var selectInCircle = Math.round(this.mapF(selector, 0,15, 0,23));
+    for (var i=0; i<this.circlePointList.length; i++) {
+        for (var j=0; j<24; j++) {
+
+            ctx.beginPath();
+            ctx.arc(this.circlePointList[i][j].x, this.circlePointList[i][j].y, 2, 0, 2 * Math.PI, false);
+            if (selectInCircle==j)
+                ctx.fillStyle = 'white';
+            else 
+                ctx.fillStyle = 'grey';
+            ctx.lineWidth = 0;
+            ctx.strokeStyle = 'black';
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
     ctx.restore();
 
+  }
+  
+  mapF(value,istart,istop,ostart,ostop) {
+   return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
   }
   
   roundRect(x, y, w, h, radius, context){ // radius is corner radius
